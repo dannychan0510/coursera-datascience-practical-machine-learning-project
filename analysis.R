47187a358d811363d539313c2fca862bfa4011ba
dannychan0510 <dannychan0510>
2015-05-27 23:22:19 +0100

    updated analysis.R

--- analysis.R
+++ analysis.R
@@ -1,6 +1,14 @@
 # Set working directory
 setwd("~/Documents/GitHub/coursera-datascience-practical-machine-learning-project")
 
-# Load in training data
-train <- read.csv("pml-training.csv")
+# Load relevant libraries
+library(caret)
 
+# Load in training and test datasets
+train <- read.csv("pml-training.csv")
+test <- read.csv("pml-testing.csv")
+
+# Create training and testing datasets
+inTrain <- createDataPartition(y = )
+
+# Need to clean dataset
